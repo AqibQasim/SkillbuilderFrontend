55289e2042a53341d1f87d73efa79d8c1e45f465
sohaib abbasi <syedzubairalam123@gmail.com>
2024-07-01 19:50:49 +0500

    development phase v1.1.0

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -3,43 +3,24 @@ import Image from "next/image";
 import Link from "next/link";
 import { useDispatch, useSelector } from "react-redux";
 import { signupUser, signupWithGoogle } from "../../redux/thunks/auththunks";
+import { useSession, signIn, signOut } from 'next-auth/react';
 // import { signupUser } from "../../redux/thunks/auththunks";
 
 const Signup = () => {
-  useEffect(() => {
-    const initializeGoogleSignIn = () => {
-      if (window.gapi) {
-        window.gapi.load("auth2", () => {
-          window.gapi.auth2
-            .init({
-              client_id:
-                "39529635224-ct7s70vmmtjpvh6gsdmhpvec1i7ol36a.apps.googleusercontent.com",
-            })
-            .then(() => {
-              console.log("Google API initialized.");
-            })
-            .catch((error) => {
-              console.error("Error initializing Google API:", error);
-            });
-        });
-      }
-    };
-
-    initializeGoogleSignIn();
-  }, []);
 
   const dispatch = useDispatch();
   const { isLoading, error, successMessage } = useSelector(
     (state) => state.auth
   );
+
   const [first_name, setfirst_name] = useState("");
   const [last_name, setlast_name] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
   const [formError, setFormError] = useState("");
-
-  const handleSubmit = (e) => {
+  
+ const handleSubmit = (e) => {
     e.preventDefault();
     const passwordCriteria =
       /^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+{}\[\]:;<>,.?~\\/-])[A-Za-z\d!@#$%^&*()_+{}\[\]:;<>,.?~\\/-]{8,}$/;
@@ -53,42 +34,17 @@ const Signup = () => {
       );
       return;
     }
-    setFormError(""); // Clear any previous error
+    setFormError(""); 
     if (password)
       dispatch(signupUser({ first_name, last_name, email, password }));
   };
 
-  const handleGoogleSignup = async (googleUser) => {
-    try {
-      const idToken = googleUser.getAuthResponse().id_token;
-      console.log("Google User ID Token:", idToken);
-      dispatch(signupWithGoogle(idToken));
-    } catch (error) {
-      console.error("Error during Google sign-up:", error);
-    }
-  };
-
-  const handleGoogleSignIn = () => {
-    if (window.gapi && window.gapi.auth2) {
-      const auth2 = window.gapi.auth2.getAuthInstance();
-      auth2
-        .signIn()
-        .then(handleGoogleSignup)
-        .catch((error) => {
-          console.error("Error during Google sign-in:", error);
-          if (error.error === "popup_closed_by_user") {
-            alert("Sign-in popup closed by user. Please try again.");
-          } else {
-            alert("An error occurred during sign-in. Please try again.");
-          }
-        });
-    } else {
-      console.error("Google API not initialized.");
-      alert(
-        "Google API not initialized. Please refresh the page and try again."
-      );
-    }
-  };
+  const { data, status } = useSession();
+  console.log("data:",data,"status:",status);
+  if (status === 'loading') return <h1> loading... please wait</h1>;
+  if (status === 'authenticated') {
+    console.log("AUTHENTICATED SUCCESSFULLY!");
+  }
 
   return (
     <div className="max-w-md w-full px-6 py-4 bg-white rounded-md shadow-md">
@@ -247,7 +203,8 @@ const Signup = () => {
           <div className="flex-grow border-t border-gray-300"></div>
         </div>
         <button
-          onClick={handleGoogleSignIn}
+          // onClick={handleGoogleSignIn}
+          onClick={() => signIn('google')}
           className="bg-white mt-4 bg-blue-700 border mb-4 w-full border-black text-black p-2 rounded-lg flex items-center justify-center"
         >
           <span className="mr-2">
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,12 +1,15 @@
 import '../styles/global.css'; // Adjust the path according to your structure
 import { Provider } from 'react-redux';
 import { store } from '../../redux/store/store';
+import { SessionProvider } from 'next-auth/react';
 
 function MyApp({ Component, pageProps }) {
-  return(
-    <Provider store={store}>
-      <Component {...pageProps} />;
-    </Provider>
+  return (
+    <SessionProvider>
+      <Provider store={store}>
+        <Component {...pageProps} />;
+      </Provider>
+    </SessionProvider>
   )
 }
 
