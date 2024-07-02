16a18ff99fb51cabec1ae17578594c4bc1b10ea8
Eloquence98 <ibneenayat1998@gmail.com>
2024-07-02 19:17:32 +0500

    add responsive styles for dynamic course page

--- src/components/CourseReviews.js
+++ src/components/CourseReviews.js
@@ -1,6 +1,7 @@
 import React from "react";
 import StarRating from "./StarRating";
 import Image from "next/image";
+import LayoutWidth from "./LayoutWidth";
 const reviews = [
   {
     id: 1,
@@ -27,39 +28,42 @@ const reviews = [
 
 const CourseReviews = () => {
   return (
-    <div className="container mt-12">
-      <h1 className="text-2xl mb-12  font-semibold max-sm:mt-[1rem]  max-sm:text-center max-sm:text-xl max-sm:w-[100%] max-sm:mb-[1rem]">
-        Course Reviews
-      </h1>
-      <div className="w-full flex flex-col justify-center items-center">
-        <div className="h-auto w-[90%] grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 place-items-center">
-          {reviews.map((review) => (
-            <div className="bg-white shadow-lg  rounded-tr-3xl rounded-bl-3xl p-8 max-w-sm">
-              <div className="flex items-center space-x-4">
-                <Image
-                  src={review.image} // Add the path to your image
-                  alt="Profile Picture"
-                  width={50}
-                  height={50}
-                  className="rounded-full"
-                />
-                <div className="p-2">
-                  <div className="flex items-center justify-between">
-                    <h3 className="text-lg font-semibold">{review.name}</h3>
-                    <p></p>
-                  </div>
-                  <div className="flex items-center">
-                    <StarRating rating={Math.round(review.rating)} />
-                    <p className="text-gray-500 text-xs	ml-4">Sep 12, 2023</p>
+    // <div className="container mt-12">
+    <LayoutWidth>
+      <div className="mt-12">
+        <h1 className="text-2xl mb-12  font-semibold max-sm:mt-[1rem]  max-sm:text-center max-sm:text-xl max-sm:w-[100%] max-sm:mb-[1rem]">
+          Course Reviews
+        </h1>
+        <div className="w-full flex flex-col justify-center items-center">
+          <div className="h-auto w-[95%] grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 place-items-center">
+            {reviews.map((review) => (
+              <div className="bg-white shadow-lg  rounded-tr-3xl rounded-bl-3xl p-4 max-w-sm">
+                <div className="flex items-center space-x-4">
+                  <Image
+                    src={review.image} // Add the path to your image
+                    alt="Profile Picture"
+                    width={50}
+                    height={50}
+                    className="rounded-full"
+                  />
+                  <div className="p-2">
+                    <div className="flex items-center justify-between">
+                      <h3 className="text-lg font-semibold">{review.name}</h3>
+                      <p></p>
+                    </div>
+                    <div className="flex items-center">
+                      <StarRating rating={Math.round(review.rating)} />
+                      <p className="text-gray-500 text-xs	ml-4">Sep 12, 2023</p>
+                    </div>
                   </div>
                 </div>
+                <p className="mt-2 text-gray-600 text-sm p-2">{review.desc}</p>
               </div>
-              <p className="mt-2 text-gray-600 text-sm p-2">{review.desc}</p>
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
       </div>
-    </div>
+    </LayoutWidth>
   );
 };
 export default CourseReviews;
