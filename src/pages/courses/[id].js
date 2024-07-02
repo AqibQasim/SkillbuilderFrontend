241e89729f807a9218a08830995cd1e88befb497
Eloquence98 <ibneenayat1998@gmail.com>
2024-07-02 14:43:17 +0500

    Revert "Merge branch 'feature-userAuthIntegration' of github.com:AqibQasim/SkillbuilderFrontend into feature-userAuthIntegration"
    This reverts commit 1d6ecba84472d30412943003ac3c62c277eaf123, reversing
    changes made to 13c10c797ef67cf00baa442332bef81b45e72064.

--- src/pages/courses/[id].js
+++ src/pages/courses/[id].js
@@ -1,13 +1,9 @@
 // pages/courses/[id]/index.js
 import { useRouter } from "next/router";
+import Image from "next/image";
 import courses from "@/data/courses";
 import Navbar from "@/components/Navbar";
-import CourseInstructor from "@/components/CourseInstructor";
-import CourseHero from "@/components/CourseHero";
-import CurrentPath from "@/components/CurrentPath";
-import Footer from "@/components/Footer";
-import CourseReviews from "@/components/CourseReviews";
-import CourseModule from "@/components/CourseModule";
+
 const CourseDetails = () => {
   const router = useRouter();
   const { id } = router.query;
@@ -19,16 +15,15 @@ const CourseDetails = () => {
 
   return (
     <>
-      <div className="h-[100%] w-[100%] bg-bg_gray">
-        <Navbar />
-        <div className="path-wrapper w-[90%] max-w-screen-2xl mx-auto mt-16 mb-8">
-          <CurrentPath />
-        </div>
-        <CourseHero />
-        <CourseInstructor />
-        <CourseModule />
-        <CourseReviews />
-        <Footer />
+      <Navbar />
+      <h1 className="text-3xl font-semibold">Course Instructor</h1>
+      
+      <div className="course-details-container">
+        <h1>{course.title}</h1>
+        <Image src={course.image} alt={course.title} width={280} height={260} />
+        <p>{course.desc}</p>
+        <p>Rating: {course.rating}</p>
+        <p>Price: ${course.price}</p>
       </div>
     </>
   );
