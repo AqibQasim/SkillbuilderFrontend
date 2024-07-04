90cbfbaab2572e37c0360e957d83e3e03a3530ee
Eloquence98 <ibneenayat1998@gmail.com>
2024-07-04 13:43:19 +0500

    fix login and signup render issue

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,25 +1,23 @@
+import Login from "@/components/Login";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/router";
-import Login from "@/components/Login";
-import { useSelector } from "react-redux";
 import { useEffect } from "react";
+import { useSelector } from "react-redux";
 
 function LoginPage() {
   const { data: session, status } = useSession();
   const { user, isLoading } = useSelector((state) => state.auth);
-  console.log("isLoading", isLoading);
-  console.log("Status", status);
   const router = useRouter();
 
-  // Check if user is authenticated
-  if (status === "authenticated" || user) {
-    router.replace("/home");
-    return null;
-  }
+  useEffect(() => {
+    // Check if user is authenticated
+    if (status === "authenticated" || user) {
+      router.replace("/home");
+    }
+  }, [status, user, router]);
 
   // Render loading state while session status is loading
   if (status === "loading" || isLoading) {
-    // for the time being use this
     return (
       <div className="flex min-h-screen items-center justify-center bg-gray-100 py-4">
         <h1>Loading... please wait</h1>
@@ -34,4 +32,5 @@ function LoginPage() {
     </div>
   );
 }
+
 export default LoginPage;
