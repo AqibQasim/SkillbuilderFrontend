6326cc3f023242a603771b63001edd4d3957b755
Eloquence98 <ibneenayat1998@gmail.com>
2024-07-05 05:15:49 +0500

    add profile thunk function

--- redux/thunks/profilethunk.js
+++ redux/thunks/profilethunk.js
@@ -3,9 +3,37 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 export const editProfile = createAsyncThunk(
   "profile/edit",
   async function (profileData) {
-    console.log("data from Thunk", profileData);
-    // PUT here
+    try {
+      const response = await fetch(
+        `${process.env.NEXT_PUBLIC_BASE_API}/update-profile`,
+        {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(profileData),
+        },
+      );
 
-    return profileData;
+      if (!response.ok) {
+        throw new Error(data.message || "Unable to update profile");
+      }
+
+      // For testing
+      //   await new Promise((res) => setTimeout(res, 6000));
+
+      const data = await response.json();
+      console.log(data);
+
+      // TODO: Uncomment to return actual data once fixed
+      // return data;
+
+      // Temporary placeholder for adding additional functionalities
+      return profileData;
+    } catch (error) {
+      return rejectWithValue(error.message || "Failed to update profile");
+    }
+
+    // return profileData;
   },
 );
