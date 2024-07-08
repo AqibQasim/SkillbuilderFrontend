7914596003cbec8312dddd68e43348a4858d7192
AqibQasim <muhammad44aqib@gmail.com>
2024-07-08 15:42:58 +0500

    dev

--- redux/slices/contactslice.js
+++ redux/slices/contactslice.js
@@ -1,5 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { submitContactForm } from "../thunks/contactThunk";
+import { submitContactForm } from "../thunks/contactthunk";
 
 const contactSlice = createSlice({
   name: "contact",
