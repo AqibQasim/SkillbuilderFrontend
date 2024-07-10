d0e300e3e4703b1ecca6671357439197b2f48e06
Eloquence98 <ibneenayat1998@gmail.com>
2024-07-10 20:10:47 +0500

    feat: add transition on user menu

--- src/components/User.js
+++ src/components/User.js
@@ -85,86 +85,90 @@ function User({ cartClickHandler, cartItemsLength }) {
               />
             )}
           </button>
-          {show && (
-            <div className="absolute right-0 top-12 z-50 mt-2 min-w-80 rounded-lg bg-white py-3">
-              <div className="mb-5 px-5">
-                {session?.user?.image ? (
-                  <img
-                    src={session?.user?.image}
-                    className="mx-auto h-16 w-16 rounded-full"
-                    alt="User avatar"
-                  />
-                ) : (
-                  <Avatar
-                    name={profile.first_name || user.first_name}
-                    className="mx-auto h-16 w-16"
-                  />
-                )}
+          <div
+            className={`absolute right-0 top-6 z-50 min-w-80 rounded-lg bg-white py-3 shadow-lg transition-all duration-300 ${
+              show
+                ? "visible translate-y-3 opacity-100"
+                : "invisible translate-y-0 opacity-0"
+            }`}
+          >
+            <div className="mb-5 px-5">
+              {session?.user?.image ? (
+                <img
+                  src={session?.user?.image}
+                  className="mx-auto h-16 w-16 rounded-full"
+                  alt="User avatar"
+                />
+              ) : (
+                <Avatar
+                  name={profile.first_name || user.first_name}
+                  className="mx-auto h-16 w-16"
+                />
+              )}
 
-                <p className="mt-2 text-center font-semibold">
-                  {session?.user?.name || user?.name}
-                </p>
-                <p className="text-center text-gray-500">
-                  {session?.user?.email || user?.email}
-                </p>
-              </div>
-              <ul className="space-y-1">
-                <li>
-                  <Link
-                    href="/my-learning"
-                    className="inline-flex w-full items-center justify-between px-5 py-2 text-gray-700 hover:bg-gray-100"
-                  >
-                    My Learning
-                    <ChevronRightIconSvg className="h-4 w-4" />
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    href="/profile"
-                    className="inline-flex w-full items-center justify-between px-5 py-2 text-gray-700 hover:bg-gray-100"
-                  >
-                    Account Settings
-                    <ChevronRightIconSvg className="h-4 w-4" />
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    href="/help"
-                    className="inline-flex w-full items-center justify-between px-5 py-2 text-gray-700 hover:bg-gray-100"
-                  >
-                    Help
-                    <ChevronRightIconSvg className="h-4 w-4" />
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    href="/purchase-history"
-                    className="inline-flex w-full items-center justify-between px-5 py-2 text-gray-700 hover:bg-gray-100"
-                  >
-                    Purchase History
-                    <ChevronRightIconSvg className="h-4 w-4" />
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    href="/become-a-tutor"
-                    className="inline-flex w-full items-center justify-between px-5 py-2 text-gray-700 hover:bg-gray-100"
-                  >
-                    Become a Tutor
-                    <ChevronRightIconSvg className="h-4 w-4" />
-                  </Link>
-                </li>
-                <li>
-                  <button
-                    onClick={handleLogout}
-                    className="mx-5 mt-5 hidden w-full items-center justify-between rounded-lg bg-blue px-4 py-2 text-white lg:flex lg:w-auto"
-                  >
-                    Log Out
-                  </button>
-                </li>
-              </ul>
+              <p className="mt-2 text-center font-semibold">
+                {session?.user?.name || user?.name}
+              </p>
+              <p className="text-center text-gray-500">
+                {session?.user?.email || user?.email}
+              </p>
             </div>
-          )}
+            <ul className="space-y-1">
+              <li>
+                <Link
+                  href="/my-learning"
+                  className="inline-flex w-full items-center justify-between px-5 py-2 text-gray-700 hover:bg-gray-100"
+                >
+                  My Learning
+                  <ChevronRightIconSvg className="h-4 w-4" />
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="/profile"
+                  className="inline-flex w-full items-center justify-between px-5 py-2 text-gray-700 hover:bg-gray-100"
+                >
+                  Account Settings
+                  <ChevronRightIconSvg className="h-4 w-4" />
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="/help"
+                  className="inline-flex w-full items-center justify-between px-5 py-2 text-gray-700 hover:bg-gray-100"
+                >
+                  Help
+                  <ChevronRightIconSvg className="h-4 w-4" />
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="/purchase-history"
+                  className="inline-flex w-full items-center justify-between px-5 py-2 text-gray-700 hover:bg-gray-100"
+                >
+                  Purchase History
+                  <ChevronRightIconSvg className="h-4 w-4" />
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="/become-a-tutor"
+                  className="inline-flex w-full items-center justify-between px-5 py-2 text-gray-700 hover:bg-gray-100"
+                >
+                  Become a Tutor
+                  <ChevronRightIconSvg className="h-4 w-4" />
+                </Link>
+              </li>
+              <li>
+                <button
+                  onClick={handleLogout}
+                  className="mx-5 mt-5 hidden w-full items-center justify-between rounded-lg bg-blue px-4 py-2 text-white lg:flex lg:w-auto"
+                >
+                  Log Out
+                </button>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     );
