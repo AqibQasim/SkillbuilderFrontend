98bc9c45f8be5fe5e9bc0542d1ea334e91d9ec8f
Eloquence98 <ibneenayat1998@gmail.com>
2024-07-30 16:59:32 +0500

    fix: add login as instructor path

--- src/pages/profession.js
+++ src/pages/profession.js
@@ -1,10 +1,29 @@
 import Fields from "@/components/Fields";
+import Loader from "@/components/Loader";
 import withAuth from "@/components/WithAuth";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchOneInstructor } from "../../redux/thunks/instructorThunk";
 
 function profession() {
+  const userId = useSelector((state) => state.auth.user);
+  const { user_id: instructorId, isInstLoading } = useSelector(
+    (state) => state.singleInstructor,
+  );
+  const dispatch = useDispatch();
+
+  useEffect(
+    function () {
+      if (!userId) return;
+      if (userId && instructorId) return;
+      dispatch(fetchOneInstructor(userId));
+    },
+    [userId, instructorId],
+  );
+
   return (
     <div className="flex min-h-screen items-center justify-center bg-gray-100">
-      <Fields />
+      {isInstLoading ? <Loader /> : <Fields />}
     </div>
   );
 }
