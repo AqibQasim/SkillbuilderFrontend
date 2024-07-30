d260e888cb815a9a53cb5b60f26c7c6a235a787a
Eloquence98 <ibneenayat1998@gmail.com>
2024-07-30 18:43:18 +0500

    fix: resolve id issue for fetchCoursesByInstructorId

--- src/pages/dashboard/instructor-courses/index.js
+++ src/pages/dashboard/instructor-courses/index.js
@@ -4,24 +4,29 @@ import withAuth from "@/components/WithAuth";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchCoursesByInstructorId } from "../../../../redux/thunks/instructorCoursesThunk";
+import { fetchOneInstructor } from "../../../../redux/thunks/instructorThunk";
 
 function Courses() {
-  const dispatch = useDispatch();
-  const instructorId = useSelector((state) => state.profile.id);
+  const userId = useSelector((state) => state.auth.user);
+  const instructorId = useSelector((state) => state.singleInstructor.id);
   const {
     courses: instructorCourses,
     isLoading,
     error,
   } = useSelector((state) => state.instructorCourses);
+  const dispatch = useDispatch();
 
   console.log("instructor Courses", instructorCourses);
   console.log("instructor Courses length", instructorCourses.length);
 
   useEffect(() => {
+    if (!instructorId) {
+      dispatch(fetchOneInstructor(userId));
+    }
     if (instructorId) {
       dispatch(fetchCoursesByInstructorId(instructorId));
     }
-  }, [dispatch, instructorId]);
+  }, [dispatch, instructorId, userId]);
 
   return (
     <DashboardLayout>
