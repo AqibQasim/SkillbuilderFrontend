f00f0aa859b357ccc79a8caadd7094c48b25617e
Eloquence98 <ibneenayat1998@gmail.com>
2024-08-13 13:59:51 +0500

    fix: remove comments on avatar component

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -1,6 +1,6 @@
 import React from "react";
-// free avatar api https://ui-avatars.com/api/?background=random&name=arda+guler
 const avatarBaseApi = "https://ui-avatars.com/api/";
+
 const Avatar = ({ firstName = "Zubai", lastName = "Alam", className }) => {
   const firstLetter = firstName?.charAt(0).toUpperCase();
 
@@ -9,9 +9,8 @@ const Avatar = ({ firstName = "Zubai", lastName = "Alam", className }) => {
       className={`${className} image-wrapper relative flex aspect-square h-12 w-12 items-center justify-center overflow-hidden rounded-full`}
     >
       <img
-        // src={`https://ui-avatars.com/api/?background=random&name=${name}arda+guler`}
         src={`${avatarBaseApi}?background=random&rounded=true&name=${firstName}+${lastName}`}
-        alt={`${name}'s avatar image`}
+        alt={`${firstName}'s avatar image`}
         className="h-full w-full rounded-full object-cover"
       />
     </div>
