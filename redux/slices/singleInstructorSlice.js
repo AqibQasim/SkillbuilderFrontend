30bc876b90296f45df414ebd3294c49e3bf03527
AqibQasim <muhammad44aqib@gmail.com>
2024-08-13 16:18:42 +0500

    dev phase

--- redux/slices/singleInstructorSlice.js
+++ redux/slices/singleInstructorSlice.js
@@ -4,17 +4,29 @@ import { fetchOneInstructor } from "../thunks/instructorThunk";
 const initialState = {
   id: null,
   user_id: null,
-  experience: "",
+  experience: [],
   specialization: "",
   video_url: "",
   status: "",
   created_at: "",
   skills: [],
-  reviews: [],
-  education: [],
   isInstLoading: false,
   InstructorError: null,
   successMessage: null,
+  user: {
+    id: null,
+    status_desc: null,
+    profile: null,
+    first_name: "",
+    email: "",
+    profession: null,
+    facebook_profile: null,
+    is_active: false,
+    role: "",
+    source: "",
+    created_at: "",
+    updated_at: "",
+  },
 };
 
 const singleInstructorSlice = createSlice({
@@ -29,7 +41,14 @@ const singleInstructorSlice = createSlice({
         state.successMessage = null;
       })
       .addCase(fetchOneInstructor.fulfilled, (state, action) => {
-        Object.assign(state, action.payload);
+        const data = action.payload; // Access the data directly
+        state.user_id = data.user_id;
+        state.experience = data.experience;
+        state.specialization = data.specialization;
+        state.video_url = data.video_url;
+        state.status = data.status;
+        state.skills = data.skills;
+        state.user = data.user;
         state.isInstLoading = false;
         state.InstructorError = null;
         state.successMessage = "Instructor fetched successfully";
