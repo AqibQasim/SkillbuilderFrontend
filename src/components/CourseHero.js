61edf92be44064846667e3a20cd1a7a26434d74e
Sanjay Kumar <sanjay1402k@gmail.com>
2024-08-20 13:02:55 +0500

    fixed responsiveness issue in CourseHero button

--- src/components/CourseHero.js
+++ src/components/CourseHero.js
@@ -5,20 +5,23 @@ import { useRouter } from "next/router";
 import LayoutWidth from "./LayoutWidth";
 import { useSelector, useDispatch } from "react-redux";
 import { addItem } from "../../redux/slices/addToCart";
+
 function CourseHero({ course }) {
   const router = useRouter();
   const { id } = router.query;
   const dispatch = useDispatch();
   const cartItems = useSelector((state) => state.cart.items);
+
   const handleAddToCart = (course) => {
     if (!cartItems.some((item) => item.id === course.id)) {
       dispatch(addItem(course));
     }
   };
+
   const isCourseAddedToCart = (course) => {
     return cartItems.some((item) => item.id === course.id);
   };
-  // const course = courses.find((course) => course.id === parseInt(id));
+
   return (
     <div className="w-full bg-white">
       <LayoutWidth>
@@ -36,38 +39,38 @@ function CourseHero({ course }) {
               >
                 {course?.learning_outcomes}
               </p>
-              {!isCourseAddedToCart(course) && (
-                <button
-                  onClick={(e) => {
-                    e.stopPropagation();
-                    handleAddToCart(course);
-                  }}
-                  className="rounded-md bg-blue px-8 py-3 font-semibold text-white transition duration-300 ease-in-out hover:bg-blue-700"
-                >
-                  Add to Cart
-                </button>
-              )}
-              {isCourseAddedToCart(course) && (
-                <button
-                  disabled
-                  className="flex items-center rounded-md bg-blue px-8 py-3 font-semibold text-white transition duration-300 ease-in-out hover:bg-blue-700"
-                >
-                  <Image
-                    src="/Check.png"
-                    width={25}
-                    height={25}
-                    alt="Check icon"
-                    className="mr-2"
-                  />
-                  Added to Cart
-                </button>
-              )}
+              <div className="flex justify-center lg:justify-start">
+                {!isCourseAddedToCart(course) && (
+                  <button
+                    onClick={(e) => {
+                      e.stopPropagation();
+                      handleAddToCart(course);
+                    }}
+                    className="rounded-md bg-blue px-8 py-3 font-semibold text-white transition duration-300 ease-in-out hover:bg-blue-700"
+                  >
+                    Add to Cart
+                  </button>
+                )}
+                {isCourseAddedToCart(course) && (
+                  <button
+                    disabled
+                    className="flex items-center rounded-md bg-blue px-8 py-3 font-semibold text-white transition duration-300 ease-in-out hover:bg-blue-700"
+                  >
+                    <Image
+                      src="/Check.png"
+                      width={25}
+                      height={25}
+                      alt="Check icon"
+                      className="mr-2"
+                    />
+                    Added to Cart
+                  </button>
+                )}
+              </div>
             </div>
           </div>
           {/* Right Section (visible on all screens) */}
           <div className="text-black hidden flex-1 lg:flex lg:items-center lg:justify-center">
-            {/* <div className="relative h-[19.5rem] w-[30.5625rem] flex justify-center items-center">
-          </div> */}
             <Image
               // src={course.image}
               src="/dummyImg.svg"
@@ -82,4 +85,5 @@ function CourseHero({ course }) {
     </div>
   );
 }
+
 export default CourseHero;
