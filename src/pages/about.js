4802617fb5b9357f882b5d76a7516c85a6ff55d8
Taha <taha.akber007@gmail.com>
2024-08-20 13:02:06 +0500

    added loader on about us and contact us page and added validation on star rating

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,6 +1,4 @@
 import Navbar from "@/components/Navbar";
-// import { useEffect } from "react";
-// import { setCurrentTab } from "@/utils/currentTabMethods";
 import AboutGoals from "@/components/AboutGoals";
 import AboutHero from "@/components/AboutHero";
 import AboutTeam from "@/components/AboutTeam";
@@ -14,32 +12,40 @@ import LayoutWidth from "@/components/LayoutWidth";
 import { useState, useEffect } from "react";
 
 const about = () => {
-  // useEffect(() => {
-  //   setCurrentTab("about");
-  // }, []);
-
   const router = useRouter();
   const [isClient, setIsClient] = useState(false);
   const courses = useSelector((state) => state.cart.items);
-  console.log("length in root file:", courses?.length)
+  console.log("length in root file:", courses?.length);
+
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     setIsClient(true);
-  }, [router?.isReady,courses]);
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 200);
+  }, [router?.isReady, courses]);
 
   if (!isClient) {
     return null;
   }
 
+  if (isLoading) {
+    return (
+      <div className="flex h-[100vh] w-[100vw] items-center justify-center bg-bg_gray">
+        <div className="loader">Loading...</div>
+      </div>
+    );
+  }
 
   return (
     <>
-      <div className="h-[100%] w-[100%] flex flex-col items-center bg-bg_gray">
+      <div className="flex h-[100%] w-[100%] flex-col items-center bg-bg_gray">
         <Navbar cartItemsLength={courses?.length} />
         <LayoutWidth>
-        <div className="path-wrapper w-[90%] max-w-screen-2xl mx-auto mt-16 mb-8">
-          <CurrentPath />
-        </div>
+          <div className="path-wrapper mx-auto mb-8 mt-16 w-[90%] max-w-screen-2xl">
+            <CurrentPath />
+          </div>
         </LayoutWidth>
         <AboutHero />
         <AboutGoals />
