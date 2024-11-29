ec54ff39945ee5a0566707e292165d14dd07f17f
Sanjay Kumar <sanjay1402k@gmail.com>
2024-11-29 16:00:39 +0500

    terms and conditions

---
+++ src/components/Conditions.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import Condition from './Condition'
+
+const Conditions = () => {
+  return (
+    <div className='flex flex-col gap-10'>
+      <Condition
+        heading={"Conditions of use"}
+        text={
+          "Our mission is to revolutionize online education by inspiring a passion for learning, empowering individuals to acquire essential skills, and cultivating a dynamic learning ecosystem. We are committed to making education accessible to all, breaking down barriers and ensuring affordability without compromising on quality."
+        }
+      />
+      <Condition
+        heading={"Conditions of use"}
+        text={
+          "Our mission is to revolutionize online education by inspiring a passion for learning, empowering individuals to acquire essential skills, and cultivating a dynamic learning ecosystem. We are committed to making education accessible to all, breaking down barriers and ensuring affordability without compromising on quality."
+        }
+      />
+      <Condition
+        heading={"Conditions of use"}
+        text={
+          "Our mission is to revolutionize online education by inspiring a passion for learning, empowering individuals to acquire essential skills, and cultivating a dynamic learning ecosystem. We are committed to making education accessible to all, breaking down barriers and ensuring affordability without compromising on quality."
+        }
+      />
+      <Condition
+        heading={"Conditions of use"}
+        text={
+          "Our mission is to revolutionize online education by inspiring a passion for learning, empowering individuals to acquire essential skills, and cultivating a dynamic learning ecosystem. We are committed to making education accessible to all, breaking down barriers and ensuring affordability without compromising on quality."
+        }
+      />
+      <Condition
+        heading={"Conditions of use"}
+        text={
+          "Our mission is to revolutionize online education by inspiring a passion for learning, empowering individuals to acquire essential skills, and cultivating a dynamic learning ecosystem. We are committed to making education accessible to all, breaking down barriers and ensuring affordability without compromising on quality."
+        }
+      />
+    </div>
+  );
+}
+
+export default Conditions
