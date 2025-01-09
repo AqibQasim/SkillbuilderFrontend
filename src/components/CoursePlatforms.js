5c6e9974a410d1fafaa905b6c235a5887b378d3a
Eloquence98 <ibneenayat1998@gmail.com>
2025-01-09 16:15:50 +0500

    fix: resolve shadow width

--- src/components/CoursePlatforms.js
+++ src/components/CoursePlatforms.js
@@ -24,8 +24,8 @@ function CoursePlatforms() {
           </p>
 
           <div className="platform-icons relative bg-white">
-            <div className="left-shadow pointer-events-none absolute bottom-0 left-0 z-[2] h-full w-24 bg-gradient-to-r from-white from-15%"></div>
-            <div className="right-shadow pointer-events-none absolute bottom-0 right-0 z-[2] h-full w-24 bg-gradient-to-l from-white from-15%"></div>
+            <div className="left-shadow pointer-events-none absolute bottom-0 left-0 z-[2] h-full w-28 bg-gradient-to-r from-white from-15%"></div>
+            <div className="right-shadow pointer-events-none absolute bottom-0 right-0 z-[2] h-full w-28 bg-gradient-to-l from-white from-15%"></div>
             <div className="icons relative z-[1] flex items-center overflow-hidden whitespace-nowrap">
               <div className={`logos flex gap-8 md:gap-10 xl:gap-16`}>
                 {images.map((image, index) => (
