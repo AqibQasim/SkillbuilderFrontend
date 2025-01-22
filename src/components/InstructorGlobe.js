cef1a5d5bc08092fe931e1ef5de41cbbfc10bd60
Eloquence98 <ibneenayat1998@gmail.com>
2025-01-22 19:43:46 +0500

    fix: add dynamic tutor data

--- src/components/InstructorGlobe.js
+++ src/components/InstructorGlobe.js
@@ -10,25 +10,10 @@ function InstructorGlobe() {
   const globeRef = useRef(null);
   const [hex, setHex] = useState({ features: [] });
 
-  console.log("MAR", markups);
-
-  const markerSvg = `<svg viewBox="-4 0 36 36">
-  <path fill="currentColor" d="M14,0 C21.732,0 28,5.641 28,12.6 C28,23.963 14,36 14,36 C14,36 0,24.064 0,12.6 C0,5.641 6.268,0 14,0 Z"></path>
-  <circle fill="black" cx="14" cy="14" r="7"></circle>
-</svg>`;
-
-  // Gen random data
-  const N = 30;
-  // const gData = [...Array(N).keys()].map(() => ({
-  //   lat: (Math.random() - 0.5) * 180,
-  //   lng: (Math.random() - 0.5) * 360,
-  //   size: 7 + Math.random() * 30,
-  //   color: ["red", "white", "blue", "green"][Math.round(Math.random() * 3)],
-  // }));
   const gData = [
     {
-      lat: -11.709616,
-      lng: -30.188601,
+      lat: 69.834573,
+      lng: -46.931788,
       pos: 0,
     },
     {
@@ -44,34 +29,40 @@ function InstructorGlobe() {
   }, []);
 
   return (
-    <Globe
-      // Globe config
-      ref={globeRef}
-      height={600}
-      animateIn={true}
-      backgroundColor="rgba(0, 0, 0, 0)"
-      globeMaterial={
-        new THREE.MeshBasicMaterial({
-          color: "#373668",
-          transparent: false,
-        })
-      }
-      // polygons
-      hexPolygonsData={hex.features}
-      hexPolygonResolution={3}
-      hexPolygonMargin={0.5}
-      hexPolygonColor={useCallback(() => "white", [])}
-      // Variant
-      htmlElementsData={gData}
-      htmlElement={(d) => {
-        const el = document.createElement("div");
-        // el.innerHTML = globeInstructorCardMarkup
-        el.innerHTML = markups.at(d.pos);
-        el.className = "globe-instructor-details";
-        el.onclick = () => console.info(d);
-        return el;
-      }}
-    />
+    <div className="wrapper-globe relative !mt-0">
+      {/* <div className="bg-[rgba(255, 255, 255, 0.7)] pointer-events-none absolute inset-0 z-40 !size-full mix-blend-screen"></div> */}
+      {/* <div className="pointer-events-none absolute inset-0 z-40 h-[600px] w-full bg-white opacity-55 mix-blend-overlay"></div>   */}
+      <Globe
+        // Globe config
+        ref={globeRef}
+        height={650}
+        animateIn={true}
+        backgroundColor="rgba(0, 0, 0, 0)"
+        globeMaterial={
+          new THREE.MeshBasicMaterial({
+            color: "#373668",
+            transparent: false,
+          })
+        }
+        atmosphereColor="#91B4F5"
+        atmosphereAltitude="0.20"
+        // polygons
+        hexPolygonsData={hex.features}
+        hexPolygonResolution={3}
+        hexPolygonMargin={0.5}
+        hexPolygonColor={useCallback(() => "white", [])}
+        // Variant
+        htmlElementsData={gData}
+        htmlElement={(d) => {
+          const el = document.createElement("div");
+          // el.innerHTML = globeInstructorCardMarkup
+          el.innerHTML = markups.at(d.pos);
+          el.className = "globe-instructor-details";
+          el.onclick = () => console.info(d);
+          return el;
+        }}
+      />
+    </div>
   );
 }
 
