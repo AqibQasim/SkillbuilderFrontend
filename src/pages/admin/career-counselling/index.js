8334161e376ba785de156245c2aa0713a6d1325c
Sanjay Kumar <sanjay1402k@gmail.com>
2025-01-22 19:50:02 +0500

    admin dashboard: live courses flow

---
+++ src/pages/admin/career-counselling/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import AdminDashboardLayout from "@/components/AdminDashboardLayout";
+
+const index = () => {
+  return (
+    <AdminDashboardLayout>
+      <div>career counselling</div>
+    </AdminDashboardLayout>
+  );
+};
+
+export default index;
