0acf2537e51b9f254be5d6d8d2050f0d465bf311
Eloquence98 <ibneenayat1998@gmail.com>
2025-01-22 17:58:47 +0500

    feat: add insturctor review card as markers on globe

--- src/components/GlobeInstructorCard.js
+++ src/components/GlobeInstructorCard.js
@@ -1,6 +1,44 @@
 import ImageCssBg from "./ImageCssBg";
+const reviews = [
+  {
+    name: "Zubair Alam",
+    role: "Mentor",
+    experience: [
+      "10+ Years of average mentor experience.",
+      "Access to live Q&A, workshops, and 1-on-1 sessions.",
+    ],
+  },
+  {
+    name: "John Doe",
+    role: "Mentor",
+    experience: [
+      "10+ Years of average mentor experience.",
+      "Access to live Q&A, workshops, and 1-on-1 sessions.",
+    ],
+  },
+];
 
-export const globeInstructorCardMarkup = `<div className="test-card bg-[rgba(255, 255, 255, 0.5)] w-max max-w-[400px] space-y-4 rounded-[1.25rem] p-4 shadow-[inset_0_0_0_2px_#F6EBEB] backdrop-blur-[7px]">
+export const markups = reviews.map((review, i) => {
+  const experienceList = review.experience
+    .map((exp) => `<li>${exp}</li>`)
+    .join("");
+  return `<div style={{backgroundColor: "red"}} className="test-card bg-[rgba(255, 255, 255, 0.5)] w-max max-w-[400px] space-y-4 rounded-[1.25rem] p-4 shadow-[inset_0_0_0_2px_#F6EBEB] backdrop-blur-[7px]">
+      <div className="person flex items-center justify-start gap-2">
+        <div className="icon-wrapper relative size-12 rounded-full bg-[#F5F5F5]">
+          <ImageCssBg src="/review_avatar.svg" alt="review avatar image" />
+        </div>
+        <div className="person-info">
+          <h3 className="font-medium text-[#170D23]"> ${review.name} </h3>
+          <p className="text-[#4A525D]">${review.role} </p>
+        </div>
+      </div>
+      <ul className="text-xs font-medium text-[#000F27]">
+      ${experienceList}
+      </ul>
+    </div>`;
+});
+
+export const globeInstructorCardMarkup = `<div style={{backgroundColor: "red"}} className="test-card bg-[rgba(255, 255, 255, 0.5)] w-max max-w-[400px] space-y-4 rounded-[1.25rem] p-4 shadow-[inset_0_0_0_2px_#F6EBEB] backdrop-blur-[7px]">
       <div className="person flex items-center justify-start gap-2">
         <div className="icon-wrapper relative size-12 rounded-full bg-[#F5F5F5]">
           <ImageCssBg src="/review_avatar.svg" alt="review avatar image" />
@@ -16,6 +54,7 @@ export const globeInstructorCardMarkup = `<div className="test-card bg-[rgba(255
       </ul>
     </div>`;
 
+// Above code is for 3d object html marker prop
 function GlobeInstructorCard() {
   return (
     <div className="test-card bg-[rgba(255, 255, 255, 0.5)] w-max max-w-[400px] space-y-4 rounded-[1.25rem] p-4 shadow-[inset_0_0_0_2px_#F6EBEB] backdrop-blur-[7px]">
--- src/components/InstructorGlobe.js
+++ src/components/InstructorGlobe.js
@@ -2,12 +2,15 @@ import React, { useState, useEffect, useRef, useCallback } from "react";
 import dynamic from "next/dynamic";
 import * as THREE from "three";
 import HEX_DATA from "../data/countries_hex_data.json";
+import { globeInstructorCardMarkup, markups } from "./GlobeInstructorCard";
 
 const Globe = dynamic(import("react-globe.gl"), { ssr: false });
 
 function InstructorGlobe() {
   const globeRef = useRef(null);
   const [hex, setHex] = useState({ features: [] });
+
+  console.log("MAR", markups);
 
   const markerSvg = `<svg viewBox="-4 0 36 36">
   <path fill="currentColor" d="M14,0 C21.732,0 28,5.641 28,12.6 C28,23.963 14,36 14,36 C14,36 0,24.064 0,12.6 C0,5.641 6.268,0 14,0 Z"></path>
@@ -16,12 +19,24 @@ function InstructorGlobe() {
 
   // Gen random data
   const N = 30;
-  const gData = [...Array(N).keys()].map(() => ({
-    lat: (Math.random() - 0.5) * 180,
-    lng: (Math.random() - 0.5) * 360,
-    size: 7 + Math.random() * 30,
-    color: ["red", "white", "blue", "green"][Math.round(Math.random() * 3)],
-  }));
+  // const gData = [...Array(N).keys()].map(() => ({
+  //   lat: (Math.random() - 0.5) * 180,
+  //   lng: (Math.random() - 0.5) * 360,
+  //   size: 7 + Math.random() * 30,
+  //   color: ["red", "white", "blue", "green"][Math.round(Math.random() * 3)],
+  // }));
+  const gData = [
+    {
+      lat: -11.709616,
+      lng: -30.188601,
+      pos: 0,
+    },
+    {
+      lat: 5.42644,
+      lng: 23.636377,
+      pos: 1,
+    },
+  ];
 
   // Countries
   useEffect(() => {
@@ -50,12 +65,9 @@ function InstructorGlobe() {
       htmlElementsData={gData}
       htmlElement={(d) => {
         const el = document.createElement("div");
-        el.innerHTML = markerSvg;
-        el.style.color = d.color;
-        el.style.width = `${d.size}px`;
-
-        el.style["pointer-events"] = "auto";
-        el.style.cursor = "pointer";
+        // el.innerHTML = globeInstructorCardMarkup
+        el.innerHTML = markups.at(d.pos);
+        el.className = "globe-instructor-details";
         el.onclick = () => console.info(d);
         return el;
       }}
