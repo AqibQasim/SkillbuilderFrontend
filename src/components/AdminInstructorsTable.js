9cfafe459a44eea8a9806e9da892dbd82de0bbab
Aahil Alwani <alwani.aahil25@gmail.com>
2025-03-13 16:45:32 +0500

    merge: skilled-ui into instructor-rights

--- src/components/AdminInstructorsTable.js
+++ src/components/AdminInstructorsTable.js
@@ -2,6 +2,7 @@ import AdminInstructorsRow from "./AdminInstructorsRow";
 import Table from "./Table";
 
 function AdminInstructorsTable({ isSpecific = false, instructors }) {
+
   if (!instructors.length)
     return (
       <div className="flex size-full flex-col items-center justify-center gap-4 text-center">
@@ -19,7 +20,14 @@ function AdminInstructorsTable({ isSpecific = false, instructors }) {
     "Purchase Date",
     "Course Progress",
   ];
-  const headerForInstructors = ["Name", "Email", "Joining Date"];
+  const headerForInstructors = [
+    "Name",
+    "Email",
+    "Joining Date",
+    "Course Rights",
+    "Bootcamp Rights",
+    "Career Counselling Rights",
+  ];
   const headers = isSpecific ? headerForOneInstructor : headerForInstructors;
 
   return (
@@ -27,11 +35,16 @@ function AdminInstructorsTable({ isSpecific = false, instructors }) {
       <div className="component-header flex items-center justify-between">
         <h2 className="text-2xl font-medium capitalize">All Instructors</h2>
       </div>
-      <Table columns="grid-cols-[2.5rem_1.1fr_1.25fr_1fr_0.25fr]">
+      <Table columns="grid-cols-[2.5rem_0.5fr_0.6fr_0.3fr_0.3fr_0.3fr_0.25fr_0.11fr]">
         <Table.Header>
           <div></div>
           {headers.map((heading, index) => (
-            <div key={index}>{heading}</div>
+            <div
+              key={index}
+              className="flex items-center justify-center text-center"
+            >
+              {heading}
+            </div>
           ))}
           <div></div>
         </Table.Header>
--- src/components/Showcase.js
+++ src/components/Showcase.js
@@ -1,15 +1,13 @@
 import Image from "next/image";
 import HomePageNavbar from "./HomePageNavbar";
 import LayoutXPadding from "./LayoutXPadding";
-import ImageCssBg from "./ImageCssBg";
-import showcaseImage from "../../public/showcase-image.png";
 
-function  Showcase() {
+function Showcase() {
   return (
     // add tmep height
     <LayoutXPadding>
       <div className="mx-3 !mt-0 rounded-[1.25rem] bg-[#F3F7FA] !px-4 !pt-4 sm:!px-[3.1rem] md:pt-11 xl:mx-5">
-        <HomePageNavbar className="!mt-0 z-[2]" />
+        <HomePageNavbar className="z-[2] !mt-0" />
         <div className="content mb-8 mt-5 flex flex-col items-center justify-center text-center md:mb-11 lg:mt-10 2xl:mt-24">
           <div className="highlight relative z-[1] w-max rounded-[2.2rem] border border-[#000DFF] bg-gradient-to-br from-[#E8E9FF] from-50% to-[rgba(193,197,255,0.5)] px-4 py-2 text-[0.9375rem] text-xs font-medium text-[#313131] md:px-5 xlg:mx-[unset]">
             Rate 4.9/5 from over 157+ review
@@ -31,15 +29,15 @@ function  Showcase() {
           style={{ perspective: "1000px" }}
         >
           {/* Shape */}
-          <div className="border-shape absolute -top-48 left-0 size-12 bg-[#2A6ED4] mix-blend-overlay hidden md:block md:-top-52 lg:-top-60 xl:size-16 2xl:-top-64"></div>
+          <div className="border-shape absolute -top-48 left-0 hidden size-12 bg-[#2A6ED4] mix-blend-overlay md:-top-52 md:block lg:-top-60 xl:size-16 2xl:-top-64"></div>
           {/* Shape2 */}
           <div className="shape-2 absolute -left-[5.6rem] -top-[4.1rem] bg-[#F3F7FA]">
             <div className="sector !size-52"></div>
             <div className="bg-[rgba(255, 255, 255, 0.5)] absolute inset-[2.3rem] size-60 rounded-full backdrop-blur-[7px]"></div>
           </div>
           {/* Shape 3 */}
-          <div className="shape-2 absolute -top-[12rem] hidden md:flex right-0 size-64 items-start justify-end bg-[#F3F7FA]">
-            <div className="border-shape-2  size-36 bg-[#2A6ED4]"></div>
+          <div className="shape-2 absolute -top-[12rem] right-0 hidden size-64 items-start justify-end bg-[#F3F7FA] md:flex">
+            <div className="border-shape-2 size-36 bg-[#2A6ED4]"></div>
             <Image
               className="absolute -right-4 -top-16"
               src="/shape-try.png"
@@ -62,8 +60,8 @@ function  Showcase() {
               src={showcaseImage}
             /> */}
 
-<video
-              className="h-full w-full rounded-t-[2rem] object-contain md:object-cover object-center "
+            <video
+              className="h-full w-full rounded-t-[2rem] object-contain object-center md:object-cover"
               src="/video/skillbuilder.mp4"
               poster="/showcase-image.png"
               controlsList="nodownload"
